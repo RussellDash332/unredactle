184f4dbc513126814c23ff56170fb31e10e9a1ce
RussellDash332 <russellsaerang@gmail.com>
2022-12-10 00:20:09 +0700

    Decouple stuff

--- gen_summary.py
+++ gen_summary.py
@@ -1,10 +1,17 @@
-import wikipedia, os
+import requests, wikipedia, os, re
 
 def gen_summary():
     os.makedirs('summaries', exist_ok=True)
-    for fn in os.listdir('articles'):
-        try:
-            summary = wikipedia.summary(fn.strip('.txt'))
-            open(os.path.join('summaries', fn), 'w+').write(summary)
-        except:
-            pass
+    src = 'https://en.wikipedia.org/wiki/Wikipedia:Vital_articles/Level/4'
+    content = requests.get(src).content.decode()
+    subtopics = re.findall('href="/wiki/Wikipedia:Vital_articles/Level/4/([^"]*)', content)
+    for subtopic in sorted(set(subtopics)):
+        subcontent = requests.get(src + '/' + subtopic).content.decode()
+        articles = sorted(set(re.findall('href="/wiki/([^":]*)"', subcontent)))
+        for article in articles:
+            try:
+                article = article.replace('/', '-')
+                summary = wikipedia.summary(article)
+                open(os.path.join('summaries', article + '.txt'), 'w+').write(summary)
+            except:
+                pass
